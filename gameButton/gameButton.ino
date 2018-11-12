fc10532312fdaaa67e6ee0da62df8fae9050d621
opheophe <opheophe@gmail.com>
2018-11-12 16:24:05 +0100

    First working version

--- gameButton/gameButton.ino
+++ gameButton/gameButton.ino
@@ -1,45 +1,138 @@
-int pin_led_1=2;
-int pin_button_1=5;
-int pin_buzzer=3;
+int cable_1_led = 11;
+int cable_1_button = 12;
+int cable_2_led = 10;
+int cable_2_button = 9;
+
+int set_button = A5;
+int reset_button = A3;
+int status = 1;
+unsigned long previous_time;
+int blink_status = 0;
+int winner = 0;
 
 void setup() {
-  //start serial connection
   Serial.begin(9600);
-  //configure pin 2 as an input and enable the internal pull-up resistor
-  pinMode(pin_button_1, INPUT_PULLUP);
-  pinMode(pin_led_1, OUTPUT);
-  pinMode(pin_buzzer, OUTPUT);  
 
+  pinMode(cable_1_button, INPUT_PULLUP);
+  pinMode(cable_1_led, OUTPUT);
+  pinMode(cable_2_button, INPUT_PULLUP);
+  pinMode(cable_2_led, OUTPUT);
+  pinMode(set_button, INPUT_PULLUP);
+  pinMode(reset_button, INPUT_PULLUP);
 }
 
 
-void buzz(int targetPin, long frequency, long length) {
-  long delayValue = 1000000 / frequency / 2; // calculate the delay value between transitions
-  //// 1 second's worth of microseconds, divided by the frequency, then split in half since
-  //// there are two phases to each cycle
-  long numCycles = frequency * length / 1000; // calculate the number of cycles for proper timing
-  //// multiply frequency, which is really cycles per second, by the number of seconds to
-  //// get the total number of cycles to produce
-  for (long i = 0; i < numCycles; i++) { // for the calculated length of time...
-    digitalWrite(targetPin, HIGH); // write the buzzer pin high to push out the diaphram
-    delayMicroseconds(delayValue); // wait for the calculated delay value
-    digitalWrite(targetPin, LOW); // write the buzzer pin low to pull back the diaphram
-    delayMicroseconds(delayValue); // wait again or the calculated delay value
-  }
+/*
 
-}
+  LOGIK
+
+  1. Nollställd
+  Inga lampor lyser
+  Ingen kan trycka
+
+  2. Primad
+  Alla lampor blinkar
+  Alla kan trycka
+  Den som trycker winner
+
+  3. Vinst
+  Lampan som tryckte är tänd
+  Alla andra släkta
+  Ingen kan trycka
+
+
+  SET
+  Sätter alltid till primad
+
+  RESET
+  Sätter alltid till nollställd
+*/
 
 void loop() {
-  int sensorVal = digitalRead(pin_button_1);
-  Serial.println(sensorVal);
 
-  if (sensorVal == LOW) {
-    digitalWrite(pin_led_1, LOW);
-  } else {
-    digitalWrite(pin_led_1, HIGH);
-    buzz(pin_buzzer, 1000, 250);
-    buzz(pin_buzzer, 3000, 250);
-    buzz(pin_buzzer, 700, 250);
-    buzz(pin_buzzer, 1000, 250);
+  switch (status) {
+    case 1:
+      // RESET, do nothing ontill SET is pressed
+      break;
+    case 2:
+      if (millis() - 100 > previous_time) {
+        previous_time = millis();
+        if (blink_status == 0) {
+          blink_status = 1;
+          digitalWrite(cable_1_led, HIGH);
+          digitalWrite(cable_2_led, HIGH);
+        } else {
+          blink_status = 0;
+          digitalWrite(cable_1_led, LOW);
+          digitalWrite(cable_2_led, LOW);
+        }
+        Serial.println("Blink");
+      }
+
+      break;
+    case 3:
+      switch (winner) {
+        case 1:
+          digitalWrite(cable_1_led, HIGH);
+          digitalWrite(cable_2_led, LOW);
+          break;
+        case 2:
+          digitalWrite(cable_1_led, LOW);
+          digitalWrite(cable_2_led, HIGH);
+          break;
+        default:
+
+          break;
+      }
+
+
+
+      break;
+    default:
+
+      break;
+  }
+
+  int buttonVal_1 = digitalRead(cable_1_button);
+  if (buttonVal_1 == LOW) {
+    Serial.println("Button 1 pressed");
+    if (status == 2) {
+      winner = 1;
+      status = 3;
+    }
+  }
+
+  int buttonVal_2 = digitalRead(cable_2_button);
+  if (buttonVal_2 == LOW) {
+    Serial.println("Button 2 pressed");
+    if (status == 2) {
+      winner = 2;
+      status = 3;
+    }
+  }
+
+
+  
+  int buttonVal_set = digitalRead(set_button);
+  if (buttonVal_set == LOW) {
+    Serial.println("Button set pressed");
+    Serial.println("Status = 2");
+    status = 2;
+    blink_status = 1;
+    previous_time = millis();
+    digitalWrite(cable_1_led, HIGH);
+    digitalWrite(cable_2_led, HIGH);
+    winner = 0;
+  }
+
+  int buttonVal_reset = digitalRead(reset_button);
+  if (buttonVal_reset == LOW) {
+    Serial.println("Button reset pressed");
+    Serial.println("Status = 1");
+    status = 1;
+    digitalWrite(cable_1_led, LOW);
+    digitalWrite(cable_2_led, LOW);
+    blink_status = 0;
+    winner = 0;
   }
 }
